ac899af4eb8c797f05862bdd5e44b9d05f3a764c
Somenath Burnwal <some.nath@outlook.in>
2024-07-18 15:14:54 +0530

    modified .py file

--- medius_tech_i.py
+++ medius_tech_i.py
@@ -13,49 +13,48 @@ class GoogleFormPage:
         self.driver.get(self.url)
 
     def fill_form(self, form_data):
-		name, contact_number, email, address, pin_code, dob, gender = form_data
-        width = self.driver.execute_script("return Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );")
-        height = self.driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
-        
-        # Set the window size to match the entire webpage
-        self.driver.set_window_size(width, height)
-        
-        # Find the full page element (usually 'body') and capture the screenshot
-        full_page = self.driver.find_element(By.TAG_NAME, "body")
-        full_page.screenshot("before_filling.png")
-        #self.driver.save_screenshot('before_filling.png')
-        
-        full_name = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i1']")
-        contact_number_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i5']")
-        email_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i9']")
-        full_address_field = self.driver.find_element(By.XPATH, "//textarea[@aria-labelledby='i13']")
-        pin_code_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i17']")
-        dob_field = self.driver.find_element(By.XPATH, "//input[@type='date']")
-        gender_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i26']")
-        captcha = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i30']")
-        
-        print("----------------pass--------------")
-        
-        full_name.send_keys(name)
-        contact_number_field.send_keys(contact_number)
-        email_field.send_keys(email)
-        full_address_field.send_keys(address)
-        pin_code_field.send_keys(pin_code)
-        dob_field.send_keys(dob)
-        gender_field.send_keys(gender)
-        captcha.send_keys("GNFPYC")
-
-        full_page = self.driver.find_element(By.TAG_NAME, "body")
-        full_page.screenshot("after_filling.png")
-        #self.driver.save_screenshot('after_filling.png')
-
-        #time.sleep(3)
-        submit_button = self.driver.find_element(By.XPATH, "//div[@aria-label='Submit']")
-        submit_button.click()
-
-        full_page = self.driver.find_element(By.TAG_NAME, "body")
-        full_page.screenshot("after_submitting.png")
-        #self.driver.save_screenshot('after_submitting.png')
+    	name, contact_number, email, address, pin_code, dob, gender = form_data
+    	width = self.driver.execute_script("return Math.max( document.body.scrollWidth, document.body.offsetWidth, document.documentElement.clientWidth, document.documentElement.scrollWidth, document.documentElement.offsetWidth );")
+    	height = self.driver.execute_script("return Math.max( document.body.scrollHeight, document.body.offsetHeight, document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight );")
+    	
+    	# Set the window size to match the entire webpage
+    	self.driver.set_window_size(width, height)
+    	# Find the full page element (usually 'body') and capture the screenshot
+    	full_page = self.driver.find_element(By.TAG_NAME, "body")
+    	full_page.screenshot("before_filling.png")
+    	#self.driver.save_screenshot('before_filling.png')
+    	
+    	full_name = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i1']")
+    	contact_number_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i5']")
+    	email_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i9']")
+    	full_address_field = self.driver.find_element(By.XPATH, "//textarea[@aria-labelledby='i13']")
+    	pin_code_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i17']")
+    	dob_field = self.driver.find_element(By.XPATH, "//input[@type='date']")
+    	gender_field = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i26']")
+    	captcha = self.driver.find_element(By.XPATH, "//input[@aria-labelledby='i30']")
+    	
+    	print("----------------pass--------------")
+    	
+    	full_name.send_keys(name)
+    	contact_number_field.send_keys(contact_number)
+    	email_field.send_keys(email)
+    	full_address_field.send_keys(address)
+    	pin_code_field.send_keys(pin_code)
+    	dob_field.send_keys(dob)
+    	gender_field.send_keys(gender)
+    	captcha.send_keys("GNFPYC")
+    	
+    	full_page = self.driver.find_element(By.TAG_NAME, "body")
+    	full_page.screenshot("after_filling.png")
+    	#self.driver.save_screenshot('after_filling.png')
+    	
+    	#time.sleep(3)
+    	submit_button = self.driver.find_element(By.XPATH, "//div[@aria-label='Submit']")
+    	submit_button.click()
+    	
+    	full_page = self.driver.find_element(By.TAG_NAME, "body")
+    	full_page.screenshot("after_submitting.png")
+    	#self.driver.save_screenshot('after_submitting.png')
 
 def test_submit_google_form(form_data):
     options = Options()
